9518596e97ea53bcf259ec27713bf85fa21f49ba
Vijay-devx <yadavvijay3107@gmail.com>
2026-08-17 08:16:46 +0530

    Add PayAuctionFeesRequest DTO

---
+++ backend/PaymentService/DTOs/PayAuctionFeesRequest.cs
@@ -0,0 +1,8 @@
+namespace PaymentService.DTOs
+{
+    public class PayAuctionFeesRequest
+    {
+        public decimal RegistrationFee { get; set; }
+        public decimal InitialPurse { get; set; }
+    }
+}
